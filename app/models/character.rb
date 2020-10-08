e4bf609edfafd4219e045d626f85121da9dc9014
Elvis Knelson <elfishpro@hotmail.com>
2020-10-08 12:06:01 -0500

    add model associations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ApplicationRecord
+  has_many :vehicles
+  belongs_to :homeworld
+  belongs_to :species
 
   validates :name, :birth_year, :homeworld, presence: true
 end
--- app/models/homeworld.rb
+++ app/models/homeworld.rb
@@ -1,2 +1,3 @@
 class Homeworld < ApplicationRecord
+  has_many :characters
 end
