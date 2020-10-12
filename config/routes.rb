584bab1e3ae0c4481f7451d23a1761a33da800cb
Elvis Knelson <elfishpro@hotmail.com>
2020-10-12 14:33:46 -0500

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'characters#index'
-  get 'characters/show'
+
+  get 'characters', to: 'characters#index'
+  resources 'characters', only: %i[index show]
+
+  get 'homeworlds', to: 'homeworlds#index'
+  resources 'homeworlds', only: %i[index show]
+
+  get 'speciess', to: 'species#index'
+  resources 'species', only: %i[index show]
 end
