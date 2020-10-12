c3d6d89e9cd83bb488db91ddbf6b57366ff5bbb9
Elvis Knelson <elfishpro@hotmail.com>
2020-10-12 14:04:53 -0500

    update character controller views

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,9 @@
 class CharactersController < ApplicationController
   def index
+    @characters = Character.includes(:species, :homeworld).all.paginate(:page => params[:page], :per_page => 12)
   end
 
   def show
+    @character = Character.find(params[:id])
   end
 end
